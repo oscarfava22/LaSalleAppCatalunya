6d5c51e0143addfe1184c1ca39d5415d06dcbab2
oscarfava22 <32568957+oscarfava22@users.noreply.github.com>
2018-04-04 00:31:10 +0200

    new registerAdmin activity

--- app/src/main/java/com/practica2/projectes2/lasalle/lasalleappcatalunya/RegisterNewAdmin.java
+++ app/src/main/java/com/practica2/projectes2/lasalle/lasalleappcatalunya/RegisterNewAdmin.java
@@ -3,11 +3,12 @@ package com.practica2.projectes2.lasalle.lasalleappcatalunya;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterNewAdmin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register_new_admin_activity);
+
     }
 }
