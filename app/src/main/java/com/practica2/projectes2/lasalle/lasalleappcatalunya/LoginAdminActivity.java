bb918fdee262b4ae8bae4ca734f0acfc3e8c28b9
RamonFarre <ramon.farre.2016@salleurl.edu>
2018-04-04 18:42:13 +0200

    S'ha creat la activity corresponent al login d'un administrador. S'ha iniciat l'opcional de inici de sessió mitjançant xarxes socials. S'ha implementat l'inici de sessió mitjançant Facebook.

---
+++ app/src/main/java/com/practica2/projectes2/lasalle/lasalleappcatalunya/LoginAdminActivity.java
@@ -0,0 +1,63 @@
+package com.practica2.projectes2.lasalle.lasalleappcatalunya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
+
+
+public class LoginAdminActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_admin);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code   
+                    }
+                });
+
+        Button fb_button = findViewById(R.id.fb_button);
+
+        fb_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(LoginAdminActivity.this, Arrays.asList("public_profile"));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
